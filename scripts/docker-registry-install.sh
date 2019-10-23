01d8c70817931724c123e9d75ab42bfcf94ff3cb
Steven Toth <stoth@kernellabs.com>
2019-10-23 10:32:42 -0400

    Changed docker to secure installation with TLS and passwords

--- scripts/docker-registry-install.sh
+++ scripts/docker-registry-install.sh
@@ -1,18 +1,25 @@
 #!/bin/bash -x
 
 # References:
+#  https://ahmermansoor.blogspot.com/2019/04/configure-secure-registry-docker-distribution-centos-7.html
 #  https://www.linuxtechi.com/setup-docker-private-registry-centos-7-rhel-7/
 #  https://ahmermansoor.blogspot.com/2019/03/configure-private-docker-registry-centos-7.html
 
-sudo yum -y install docker
+yum -y install docker
 
-sed -i --follow-symlinks 's!seccomp.json \\!seccomp.json --insecure-registry docker-registry.example.com:5000 \\!g' /usr/lib/systemd/system/docker.service
+if [ ! -d /etc/docker/certs.d/docker-distribution-01.example.com:5000 ]; then
+  mkdir -p /etc/docker/certs.d/docker-registry.example.com:5000
+  echo "We need the private key from our docker server, enter the root password and we'll SCP it"
+  scp root@docker-registry:/etc/pki/tls/registry.crt /etc/docker/certs.d/docker-registry.example.com\:5000/
+fi
 
-systemctl daemon-reload
-systemctl restart docker
-systemctl enable docker
+docker pull alpine
+docker tag alpine docker-registry.example.com:5000/alpine
 
-docker pull docker-registry.example.com:5000/busybox
+# You can test a push of the newly tagged alpine to our private repro with:
+#docker login  docker-registry.example.com:5000
+#docker push   docker-registry.example.com:5000/alpine
+#docker logout docker-registry.example.com:5000
 
 echo
-echo "private INSECURE dockery registry references installed"
+echo "private SECURE dockery registry references installed"
